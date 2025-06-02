84d73448a995d7ad914529b529e7ea505f1badfb
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-06-02 12:56:15 +0600

    Learning Class

---
+++ src/class.ts
@@ -0,0 +1,24 @@
+{
+  //Class
+
+  class Animal {
+    // name: string;
+    // species: string;
+    // age: string | number;
+
+    constructor(
+      public name: string, //PARAMETER PROPERTIES: I uses public in the type defination, so I don't need to write type and initialized it. the ts compiler automatically do it. It's called perameter properties
+      public species: string,
+      public age: string | number
+    ) {
+      // this.name = name;
+      // this.age = age;
+      // this.species = species;
+    }
+    makeSound() {
+      console.log(`The ${this.name} is ${this.age} years old`);
+    }
+  }
+  const cat = new Animal("Oreo", "cat", 0.4);
+  cat.makeSound();
+}
