3777e4eb1539e1876f692041a200efbbc9fc0d1a
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-06-04 05:45:30 +0600

    typeguard practice (type in)

--- src/typeguard.ts
+++ src/typeguard.ts
@@ -1,13 +1,40 @@
 {
-  //  type guard is a technique using at typescript that dealing with the variable types and narrowing it.
-  const add = (
-    param1: number | string,
-    param2: number | string
-  ): number | string => {
+  //  type guard is a technique using at typescript that dealing with the variable types and narrowing it.There is a number of typeguards available in ts.
+
+  //typeof
+  type alphaNeumeric = number | string;
+  const add = (param1: alphaNeumeric, param2: alphaNeumeric): alphaNeumeric => {
     if (typeof param1 === "number" && typeof param2 === "number") {
       return param1 + param2;
     } else {
       return param1.toString() + param2.toString();
     }
   };
+
+  //type in
+  type NormalUser = {
+    name: string;
+  };
+  type AdminUser = {
+    name: string;
+    role: string;
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ("role" in user) {
+      console.log(`my name is ${user.name} and my role is ${user.role}`);
+    } else {
+      console.log(`my name is ${user.name}`);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name: "Mohsin",
+  };
+  const adminUser: AdminUser = {
+    name: "Mohsin",
+    role: "admin",
+  };
+  getUser(normalUser);
+  getUser(adminUser);
 }
