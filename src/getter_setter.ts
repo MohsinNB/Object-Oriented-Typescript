36fcfeca8dd80864d49b114711c002eb8432a477
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-06-06 07:22:09 +0600

    learning getter setter

--- src/getter_setter.ts
+++ src/getter_setter.ts
@@ -6,6 +6,7 @@
       public readonly id: number,
       private _balance: number
     ) {}
+    /*
     addDeposit = (amount: number): number => {
       this._balance = this._balance + amount;
       return this._balance;
@@ -16,16 +17,24 @@
     cutBalance = (amount: number) => {
       return (this._balance = this._balance - amount);
     };
+    */
+
+    //getter
+    get balance() {
+      return this._balance;
+    }
+
+    // setter
+    set newDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
   }
 
   const poorAccount = new BanckAccount("Tarik", 22, 2000);
   const richAccount = new BanckAccount("Habiba", 12, 20000);
-  const newAmount1 = poorAccount.addDeposit(2000);
-  const newAmount2 = richAccount.addDeposit(2000);
 
-  const newAmount3 = poorAccount.getBalance();
-  //   console.log(poorAccount.name);
-  //   console.log(richAccount.name);
-  //   console.log(newAmount1, newAmount2);
-  console.log(newAmount3);
+  const preBalance = poorAccount.balance;
+  const currentBalance = (poorAccount.newDeposit = 50);
+
+  console.log(preBalance, currentBalance);
 }
