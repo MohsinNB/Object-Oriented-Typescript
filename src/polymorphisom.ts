30f2290945db92c0f4ecc242aa0d3acd39366a41
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-06-13 22:33:30 +0600

    polymorphisom done

--- src/polymorphisom.ts
+++ src/polymorphisom.ts
@@ -24,9 +24,43 @@
   const person1 = new Person();
   const person2 = new Student();
   const person3 = new Developer();
-  //
 
   getSleepingHours(person1);
   getSleepingHours(person2);
   getSleepingHours(person3);
+
+  class Shape {
+    getArea(): number {
+      return 0;
+    }
+  }
+
+  class circle extends Shape {
+    constructor(public radius: number) {
+      super();
+    }
+    getArea(): number {
+      return Math.PI * this.radius * this.radius;
+    }
+  }
+  class Rectengle extends Shape {
+    constructor(public width: number, public height: number) {
+      super();
+    }
+    getArea(): number {
+      return this.height * this.width;
+    }
+  }
+  const getShapeArea = (param: Shape) => {
+    console.log(param.getArea());
+  };
+
+  const shape1 = new Shape();
+  const shape2 = new circle(14);
+  const shape3 = new Rectengle(3, 4);
+
+  getShapeArea(shape1);
+  getShapeArea(shape2);
+  getShapeArea(shape3);
+  //
 }
