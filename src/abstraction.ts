94e65109d243bfa5a7a8fa7a0addae56f9f33900
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-06-14 06:18:01 +0600

    practicing abstraction

--- src/abstraction.ts
+++ src/abstraction.ts
@@ -8,10 +8,34 @@
   class car implements Vehicle1 {
     constructor(public name: string, public model: number) {}
     move(): void {
-      console.log(`The car is moving`);
+      console.log(`The ${this.name} is moving`);
     }
   }
 
   const MyCar = new car("Bugatti", 200);
   console.log(MyCar.move());
+
+  //Another example of real life implementation of Abstraction
+
+  abstract class paymentProcessor {
+    abstract Pay(amount: number): void;
+
+    validation(): void {
+      console.log(`The payment is validate`);
+    }
+  }
+
+  class payPal extends paymentProcessor {
+    Pay(amount: number): void {
+      console.log(`Amount ${amount} paid successfully`);
+    }
+  }
+  const processPayment = (processor: paymentProcessor, amount: number) => {
+    processor.validation();
+    processor.Pay(amount);
+  };
+
+  const paypal = new payPal();
+  console.log(processPayment(paypal, 72220));
+  //
 }
